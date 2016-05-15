2a296c15ac6edcbf9f113554a89f1ccd780e5a6e
wipingu <mail@joernmohr.de>
2016-05-15 14:03:30 +0200

    Update index.php
    Changed if httpheaders does not exist

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <?php
-// Echoes all http headers
-echo "<html><head></head><body>";
-echo "<h1><center>Http-Header Displaytool</center></h1><hr>";
-echo "<br>";
-try {
-    foreach (getallheaders() as $name => $value) {
-        echo $name . " : " . $value . "<br>";
-    }
-}catch(Exception $e)
-{
-    echo "Couldn't getallheaders()" . $e->getMessage();
+if( !function_exists('apache_request_headers') ) {
+        function apache_request_headers() {
+                $arh = array();
+                $rx_http = '/\AHTTP_/';
+                foreach($_SERVER as $key => $val) {
+                        if( preg_match($rx_http, $key) ) {
+                                $arh_key = preg_replace($rx_http, '', $key);
+                                $rx_matches = array();
+                                // do some nasty string manipulations to restore the original letter case
+                                // this should work in most cases
+                                $rx_matches = explode('_', strtolower($arh_key));
+                                if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+                                        foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
+                                        $arh_key = implode('-', $rx_matches);
+                                }
+                                $arh[$arh_key] = $val;
+                        }
+                }
+                if(isset($_SERVER['CONTENT_TYPE'])) $arh['Content-Type'] = $_SERVER['CONTENT_TYPE'];
+                if(isset($_SERVER['CONTENT_LENGTH'])) $arh['Content-Length'] = $_SERVER['CONTENT_LENGTH'];
+                return( $arh );
+        }
 }
-echo "</body></html>\"";
+
+$arr_headers = apache_request_headers();
+
+echo "<table border=\"1\"><tr><th>HTTP Header</th><th>Wert</th></tr>";
+ 
+foreach ($arr_headers as $nr => $os)
+{
+    echo "<tr><td>$nr</td><td>$os</td></tr>";
+}
+ 
+echo "</table>";
 ?>
